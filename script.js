c1aab6a07410ae67571f93385a2ec390dd637e4f
Ismayil <kazovkavkaz@gmail.com>
2024-12-21 14:24:56 +0400

    1212

---
+++ script.js
@@ -0,0 +1,4 @@
+var fExchange = +prompt("Zehmet olmasa USD ile meblegi daxil edin: ")
+var exchange = fExchange * 1.7
+
+document.getElementById('exchange').innerHTML = fExchange + ' USD='+ exchange + ' AZN'
